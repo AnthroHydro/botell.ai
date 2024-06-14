431a36c38250a801ec51fc4c1416249cc452b46b
abh037 <69530041+abh037@users.noreply.github.com>
2024-06-14 10:45:16 -0400

    Delete demo.py

--- demo.py
+++
@@ -1,9 +0,0 @@
-from yolo import YOLO
-
-model = YOLO(weights="bestbottle.pt", thresh=0.05)
-
-#the tracker below is optional, if no tracker is created, the model will use a default one
-#tracker = Tracker(distance_function="euclidean", distance_threshold=50, reid_distance_threshold=50, hit_counter_max=30, initialization_delay=15)
-
-model.run(path='vids/demo.mp4')
-
